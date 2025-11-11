fb125dc9c1dd8b525165de7e8cbf19f078a23bd8
Abdullahi Olalekan Abdulmumeen <olalekanabdulmumeen3@gmail.com>
2025-11-11 16:17:25 +0100

    Modify moveit confiuration using the setup assistant

--- ufactory_xarm7_moveit_config/launch/demo.launch.py
+++ ufactory_xarm7_moveit_config/launch/demo.launch.py
@@ -3,7 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder(
-        "ufactory-xarm7-robot", package_name="ufactory_xarm7_moveit_config"
-        ).to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("ufactory-xarm7-robot", package_name="ufactory_xarm7_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
